d3b138069e758f1f645dc7d45ad756770c0bc4c9
majorn <majorn@uw.edu>
2020-03-08 18:22:32 -0700

    update from Jinrong

--- Input_matrix_generation.py
+++ Input_matrix_generation.py
@@ -20,14 +20,13 @@ def create_input_matrix(data):
 
 
 def normalize_input_matrix(data):
-    data1 = create_input_matrix(data)
-    for i in range(len(data1)):
-        data1[i][0] = (data1[i][0])/20*255
-        data1[i][1] = (data1[i][1]+4.5)/9*255
-        data1[i][2] = (data1[i][2])/227.8*255
-        data1[i][3] = (data1[i][3])/4*255
-        data1[i][4] = (data1[i][4]-8)/4*255
-    return data1
+    for i in range(data.shape[1]):
+        data[0][i] = (data[0][i])/20*255
+        data[1][i] = (data[1][i]+4.5)/9*255
+        data[2][i] = (data[2][i])/227.8*255
+        data[3][i] = (data[3][i])/4*255
+        data[4][i] = (data[4][i]-8)/4*255
+    return data
 
 def sequence_to_matrix(sequence):
     import pandas as pd
@@ -49,28 +48,27 @@ def sequence_to_matrix(sequence):
 
 def hydropathy(letter):
     hydro = {
-        'I': 4.5,
-        'V': 4.2,
-        'L': 3.8,
-        'F': 2.8,
-        'C': 2.5,
-        'M': 1.9,
-        'A': 1.8,
-        'G': -0.4,
-        'T': -0.7,
-        'S': -0.8,
-        'W': -0.9,
-        'Y': -1.3,
-        'P': -1.6,
-        'H': -3.2,
-        'E': -3.5,
-        'Q': -3.5,
-        'D': -3.5,
-        'N': -3.5,
-        'K': -3.9,
-        'R': -4.5,
-        'X': 0
-    }
+     'A': 1.8,
+     'C': 2.5,
+     'D': -3.5,
+     'E': -3.5,
+     'F': 2.8,
+     'G': -0.4,
+     'H': -3.2,
+     'I': 4.5,
+     'K': -3.9,
+     'L': 3.8,
+     'M': 1.9,
+     'N': -3.5,
+     'P': -1.6,
+     'Q': -3.5,
+     'R': -4.5,
+     'S': -0.8,
+     'T': -0.7,
+     'V': 4.2,
+     'W': -0.9,
+     'Y': -1.3,
+     'X': 0}
     return hydro.get(letter,"Invalid sequence")
 
 
@@ -79,27 +77,27 @@ def hydropathy(letter):
 
 def sequence_to_num(letter):
     num = {
-        'I': 8,
-        'V': 18,
-        'L': 10,
-        'F': 5,
-        'C': 2,
-        'M': 11,
-        'A': 1,
-        'G': 6,
-        'T': 17,
-        'S': 16,
-        'W': 19,
-        'Y': 20,
-        'P': 13,
-        'H': 7,
-        'E': 4,
-        'Q': 14,
-        'D': 3,
-        'N': 12,
-        'K': 9,
-        'R': 15,
-        'X': 0
+     'A': 1,
+     'C': 2,
+     'D': 3,
+     'E': 4,
+     'F': 5,
+     'G': 6,
+     'H': 7,
+     'I': 8,
+     'K': 9,
+     'L': 10,
+     'M': 11,
+     'N': 12,
+     'P': 13,
+     'Q': 14,
+     'R': 15,
+     'S': 16,
+     'T': 17,
+     'V': 18,
+     'W': 19,
+     'Y': 20,
+     'X': 21  
     }
     return num.get(letter,"Invalid sequence")
 
@@ -109,28 +107,27 @@ def sequence_to_num(letter):
 
 def volume(letter):
     vol = {
-        'I': 166.7,
-        'V': 140.0,
-        'L': 166.7,
-        'F': 189.9,
-        'C': 108.5,
-        'M': 162.9,
-        'A': 88.6,
-        'G': 60.1,
-        'T': 116.1,
-        'S': 89.0,
-        'W': 227.8,
-        'Y': 193.6,
-        'P': 112.7,
-        'H': 153.2,
-        'E': 138.4,
-        'Q': 143.8,
-        'D': 111.1,
-        'N': 114.1,
-        'K': 168.6,
-        'R': 173.4,
-        'X': 0
-    }
+     'A': 88.6,
+     'C': 108.5,
+     'D': 111.1,
+     'E': 138.4,
+     'F': 189.9,
+     'G': 60.1,
+     'H': 153.2,
+     'I': 166.7,
+     'K': 168.6,
+     'L': 166.7,
+     'M': 162.9,
+     'N': 114.1,
+     'P': 112.7,
+     'Q': 143.8,
+     'R': 173.4,
+     'S': 89.0,
+     'T': 116.1,
+     'V': 140.0,
+     'W': 227.8,
+     'Y': 193.6,
+     'X': 0}
     return vol.get(letter,"Invalid sequence")
 
 
@@ -139,28 +136,27 @@ def volume(letter):
 
 def polarity(letter):
     polar = {
-        'I': 1,
-        'V': 1,
-        'L': 1,
-        'F': 1,
-        'C': 2,
-        'M': 2,
-        'A': 1,
-        'G': 1,
-        'T': 2,
-        'S': 2,
-        'W': 2,
-        'Y': 2,
-        'P': 1,
-        'H': 4,
-        'E': 3,
-        'Q': 2,
-        'D': 3,
-        'N': 2,
-        'K': 4,
-        'R': 4,
-        'X': 0
-    }
+     'A': 1,
+     'C': 2,
+     'D': 3,
+     'E': 3,
+     'F': 1,
+     'G': 1,
+     'H': 4,
+     'I': 1,
+     'K': 4,
+     'L': 1,
+     'M': 2,
+     'N': 2,
+     'P': 1,
+     'Q': 2,
+     'R': 4,
+     'S': 2,
+     'T': 2,
+     'V': 1,
+     'W': 2,
+     'Y': 2,
+     'X': 0}
     return polar.get(letter,"Invalid sequence")
 
 
