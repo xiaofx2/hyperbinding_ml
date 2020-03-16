66455cd4ef4700efd0a5961c582c6c9861c3831e
majorn <majorn@uw.edu>
2020-03-15 22:16:43 -0700

    rm

--- HyperBinding/single_peptide_convert.py
+++
@@ -1,6 +0,0 @@
-def peptide_converter(sequence):
-    index = int(len(sequence)/2)
-    num_X = 12 - int(len(sequence))
-    str_X = 'X'*num_X
-    res_str = sequence[:index] + str_X + sequence[index:]
-    return res_str
