5993575c85edc191913006e9a8e91f8c5b9b9bd5
jingyx6 <jingyx6@uw.edu>
2020-03-16 23:16:25 -0700

    update markdown

--- hyperbinding/peptide_cutter.py
+++ hyperbinding/peptide_cutter.py
@@ -1,3 +1,23 @@
+"""
+================================
+For application purpose:
+This file will take a protein sequence as input.
+The protein sequence will be slice into short peptides with assigned lenth.
+================================
+T cells recognize fragments of protein antigens that have been partly degraded 
+inside the antigen-presenting cell. The peptide fragments are then carried to 
+the surface of the presenting cell on special molecules called MHC proteins, 
+which present the fragments to T cells. For the Class I MHC, the binding groove 
+is optimal for peptides with 8 to 12 amino acids. Using this tool, a long 
+protein sequence can be sliced into short peptides for binding affinity prediction.
+"""
+
+
+
+###############################################################################
+# The input is protein sequence and ideal length
+# The output is a list of sliced peptides
+# -------------------
 def cutter(sequence, length):
     frag = []
     for i in range(len(sequence)-length+1):
